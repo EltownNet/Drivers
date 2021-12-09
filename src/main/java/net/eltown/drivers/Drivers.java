400a0a036e8a27257ab2d892930ad451ea274b02
Proxma <justin.wehrmeister@outlook.com>
2021-12-09 15:00:59 +0100

    Done

---
+++ src/main/java/net/eltown/drivers/Drivers.java
@@ -0,0 +1,17 @@
+package net.eltown.drivers;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Drivers extends JavaPlugin {
+
+    @Override
+    public void onLoad() {
+        // Nothing to do
+    }
+
+    @Override
+    public void onEnable() {
+        // Nothing to do
+    }
+
+}
